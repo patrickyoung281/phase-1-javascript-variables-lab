95d6e89693e5e77400604db69df6bdf7eea915c2
Patrick Young <patrickyoung281@gmail.com>
2022-04-25 14:46:17 -0400

    commit tree d321f624de7db3271450a1dd5569b139256a61a4

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution in this file!
+
+
+const companyName = "Scuber";
+
+let mostProfitableNeighborhood;
+
+mostProfitableNeighborhood = "Chelsea"
+
+let companyCeo;
+
+companyCeo= "Susan Smith";
+
